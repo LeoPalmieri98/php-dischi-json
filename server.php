ac44aa67617a099ed51d5c605e6346374ef7d557
leopii98 <rippergames98@gmail.com>
2026-04-21 19:18:43 +0200

    add-newcover

--- server.php
+++ server.php
@@ -1,8 +1,35 @@
 <?php
+//Controlliamo se tutto il form e` stato inviato
+if ( $_POST["titolo"] && $_POST["artista"] && $_POST["url_cover"] && $_POST["anno"] && $_POST["genere"]){
+ 
+    $titolo = $_POST["titolo"];   
+    $artista = $_POST["artista"]; 
+    $url_cover = $_POST["url_cover"];
+    $anno = $_POST["anno"];
+    $genere = $_POST["genere"];  
 
 
-$titolo = $_POST["titolo"];
+//Recupero i dati dentro il file.json e converto i dati con il json_decode
+$dischi_json = file_get_contents ("./dischi.json");
+$dischi_stringsNuovo = json_decode($dischi_json, true);
 
+//Aggiungo l`array recuperato dal form
+$dischi_stringsNuovo [] = [
+    "titolo" =>  $titolo,
+    "artista" =>  $artista,
+    "url_cover" =>  $url_cover,
+    "anno" =>  $anno,
+    "genere" =>  $genere
+];
 
-echo $titolo
+//Convertiamo l`array recuperato in json
+$Nuovodischi_json = json_encode($dischi_stringsNuovo);
+
+//Sovrascrivo i dati nel file .json con i dati aggiungi dal form
+file_put_contents("./dischi.json",$Nuovodischi_json);
+
+//Reinderizziamo alla pagina di home
+header("Location: ./index.php");
+};
+
 ?>
